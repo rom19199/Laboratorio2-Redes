d98876263b3c85c3d3768a3ee0b6341fc84b85a8
Hugo <Hugo_a_roman@hotmail.com>
2022-08-01 11:34:16 -0600

    Str to binary

--- Emisor.py
+++ Emisor.py
@@ -1,3 +1,4 @@
+from bitarray import bitarray
 
 #Clase donde se recibe la cadena de texto de entrada
 class Aplicacion:
@@ -7,16 +8,32 @@ class Aplicacion:
         #self.cadena = input("Escriba la cadena de texto: ")
         self.texto = texto
         
-        
-        
     def mensaje(self):
-        print(f"El mensaje es, {self.texto}")
+        #print(f"El mensaje es, {self.texto}")
+        return self.texto
+        
         
 
-class Verificacion:
-    pass
+class Verificacion(Aplicacion):
+     #Covertir str to binary ascii
+     def str2binary(self):
+        res = ''.join(format(ord(i), '08b') for i in self.texto)
+        return str(res)
+    
+    #  def bitarray(self):
+    #      arr = bitarray()      
+    #      #str2ba = ba.frombytes(f"{self.texto}".encode('utf-8'))
+    #      str2ba = arr.frombytes({self.texto}.encode('utf-8'))
+    #      return str2ba
+        
 
+
+#input del str
 cadena = input("Ingrese la cadena: ")
 text = Aplicacion(cadena)
-text.mensaje()
+texto = text.mensaje()
+print("la cadena es: ", texto)
+
+verificacion = Verificacion(texto)
+print("bin", verificacion.str2binary())   
 
